015bae3255121a926d999a2d8feae10ecfa5ec5d
moktarul12 <moktarul@gmail.com>
2024-02-26 23:02:10 +0530

    all files

---
+++ frontend/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store.js';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './themes/theme';
+import './assets/scss/style.scss';
+import App from './App';
+
+// TODO: move axios to separate instance. This is temporary
+import axios from 'axios';
+//axios.defaults.baseURL = 'https://be-fashion-shop.onrender.com/';
+axios.defaults.baseURL = 'http://localhost:3001/';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </Provider>,
+  document.getElementById('root')
+);
